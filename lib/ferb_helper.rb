a701ed8615e443d072fb713a60883c7e6ec9fc01
Jim Powers <jim@nomad-phat.powershouse.bogus>
2009-01-12 14:22:28 -0500

    Oops!  Forgot to add reasonable default for the ApplicationController

--- lib/ferb_helper.rb
+++ lib/ferb_helper.rb
@@ -19,7 +19,7 @@ module FerbHelper
   def self.included(base)
     base.send :include, Ferb
     path_suffix = ''
-    if base.name =~ /^ApplicationHelper$/i
+    if base.name =~ /^ApplicationHelper|ApplicationController$/i
       path_suffix = 'shared_templates'
     elsif base.name =~ /Helper$/i
       path_suffix = "#{base.name.gsub(/Helper$/,'').underscore}/"
